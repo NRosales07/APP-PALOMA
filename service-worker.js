fe3296856f4052acfc001685470eab8b470ad173
NRosales07 <rosalesjrnain07@gmail.com>
2026-06-30 13:59:50 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
   funcione instalado y sin conexión, como un regalo que vive en su teléfono.
 */
 
-const CACHE_NAME = "cantares-6-9-v6";
+const CACHE_NAME = "cantares-6-9-v7";
 
 const ARCHIVOS_CORE = [
   "./",
